21cd573fce3dcf75c15ce7b79134e7eaa35936d4
Mahesh Kakde <maheshkakde100@gmail.com>
2022-09-18 13:53:54 +0530

    demo deployment

---
+++ app2.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('1main.html')
+    return render_template('1main.js')
+    return render_template('1main.css')
+
+@app.route('/')
+def category():
+    return render_template('2category.html')
+    return render_template('style2.css')
+    return render_template('script2.js')    
+    
+if __name__ == '__main__':
+    #app.debug = True
+    #app.run()
+    app.run(debug=True)
---
+++ templates/assets/js/scrip2.js
@@ -0,0 +1,19 @@
+/*<!DOCTYPE html>
+<html>
+<body>
+
+<h1>HTML DOM Events</h1>
+<h2>The onclick Event</h2>
+
+
+
+
+</body>
+</html>  */
+
+
+
+
+function myFunction() {
+  document.getElementById("SPORTS").innerHTML = "Hello World";
+}
